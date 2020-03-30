584acdf3d26dbd91ebb666b72a841a403e07b92c
Ubuntu <chandlertackett@ubuntu2.5ih0wisop1aufnzrfq5t2tfu2e.gx.internal.cloudapp.net>
2020-03-30 22:02:17 +0000

    updated echoArgs.js

--- nodeFiles/echoArgs.js
+++ nodeFiles/echoArgs.js
@@ -1,5 +1,12 @@
 var http = require('http');
-var argArray = process.argv.slice(2);
+var args = process.argv.slice(2)
+var argStr = ""
+
+args.forEach(function(arg){
+  argStr = argStr.concat(' ', arg);
+})
+
+console.log(argStr);
 
 http.createServer(function(req, res) {
   res.writeHead(200, {
@@ -8,7 +15,7 @@ http.createServer(function(req, res) {
   res.write('<!doctype html>\n<html lang="en">\n' +
     '\n<meta charset="utf-8">\n<title>The Quest is Complete</title>\n' +
     '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
-    '\n\n<h1>echoing: ' + argArray[0] + '</h1>\n' +
+    '\n\n<h1>echoing: ' + argStr + '</h1>\n' +
     '\n\n');
   res.end();
 }).listen(1313);
